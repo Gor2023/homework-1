c5820c4e915acdb456b9674c9a2bc88156cb96a4
Gor2023 <132002407+Gor2023@users.noreply.github.com>
2023-05-02 17:25:49 +0400

    Add files via upload

---
+++ Script2.js
@@ -0,0 +1,17 @@
+ 1)     let user = 'Gor'
+           , age = 23 ;
+
+  
+  2)   const a = 14
+       const b = 5
+
+       const sum = a + b
+      const dif = a - b
+
+
+  3)   for (let i = 0; i < 78756; i = i + 6)  {
+    console.log(i) 
+}
+VM239:2 
+
+
